32819aed970a59ca24c666c59da231490359d0c3
AnastasiiaLukianets <anastasiia.lukianets@isolutions.ch>
2022-09-02 17:38:15 +0200

    initial commit - successful build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+name = 'Anastasiia';
+
 function App() {
 
   console.log("Hola");
 
   return (
     <div className="App">
-      Hola
+      Hola, {name}
     </div>
   );
 }
